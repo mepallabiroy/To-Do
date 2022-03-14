4fa21b11d34c5a39b913ad2f4919309dcfe3c359
Pallabi <mepallabiroy@gmail.com>
2022-03-14 23:39:53 +0530

    added config files

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'react-app',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['prettier', 'react'],
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true, parser: 'flow' }],
+  },
+};
